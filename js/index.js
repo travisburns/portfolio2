fdb35f0d8a648a945026bab26b133210dad3e54a
travisburns <travisjb0@gmail.com>
2021-08-30 11:54:17 -0700

    Updated look and feel of portfolio

--- js/index.js
+++ js/index.js
@@ -43,7 +43,7 @@ function navbarFixed(){
     if($('.header_area').length){
         $(window).scroll(function(){
             let scroll=$(window).scrollTop();
-            if(scroll>=nav_offset_top){
+            if(scroll >= nav_offset_top){
                 $('.header_area .main-menu').addClass('navbar_fixed');
             }else{
                 $('.header_area .main-menu').removeClass('navbar_fixed');
